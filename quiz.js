6da8484791c5a85ab78794b07a8789a1cea47de6
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-04-04 15:08:59 +0100

    x

---
+++ quiz.js
@@ -0,0 +1,9 @@
+
+// 1.Many forms.
+// 2.means objects can take on different from  or behaviors depending on context. different classes can be treated as if they are the same object.
+//  method that returns a different string depending on the context of where it is called.
+// 3.Interfaces.
+// 4.as many forms as it is implemented in classes.
+// 5. When many vendors within a them park have items to sell a sell interface could be made with a sell function that would implement interface and provide its own implementation of this method.
+// 6. when an object has attributes that are themselves objects.
+// 7.
